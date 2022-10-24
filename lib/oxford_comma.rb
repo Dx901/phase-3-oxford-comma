d56dbe58837f7cb186f28576567c1ccba92d422a
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-10-24 08:53:14 +0300

    DONE

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+        #The join method converts the array elements into a string
+    
+    elsif array.length == 2
+        array.join (" and ")
+    else
+        lastStr = array.pop
+        #Take the last element out
+        array.join(", ")<<", and #{lastStr}"
+    end
 end
